02a9155497a9c3c310799342cc447cae01ba911d
hankun <ihankun@outlook.com>
2024-01-07 01:48:22 +0800

    feature: (1.4.1) 并行框架支持

---
+++ spring-commons/src/main/java/io/ihankun/framework/common/async/callback/DefaultCallback.java
@@ -0,0 +1,21 @@
+package io.ihankun.framework.common.async.callback;
+
+
+import io.ihankun.framework.common.async.worker.WorkResult;
+
+/**
+ * 默认回调类，如果不设置的话，会默认给这个回调
+ * @author hankun wrote on 2019-11-19.
+ */
+public class DefaultCallback<T, V> implements ICallback<T, V> {
+    @Override
+    public void begin() {
+
+    }
+
+    @Override
+    public void result(boolean success, T param, WorkResult<V> workResult) {
+
+    }
+
+}
