a4519775c02d51df763917fc322325dec0c15c7b
韩坤 <hankun@msunhealth.com>
2024-04-12 15:14:55 +0800

    feature(2.0.0): 重构

--- spring-captcha-starter/src/main/java/io/ihankun/framework/captcha/v2/generator/impl/SliderGeneratorImpl.java
+++ spring-captcha-starter/src/main/java/io/ihankun/framework/captcha/v2/generator/impl/SliderGeneratorImpl.java
@@ -1,16 +1,22 @@
 package io.ihankun.framework.captcha.v2.generator.impl;
 
+import io.ihankun.framework.captcha.v2.generator.AbstractGenerator;
 import io.ihankun.framework.captcha.v2.generator.GeneratorInterface;
 import io.ihankun.framework.captcha.v2.generator.entity.CaptchaGeneratorVO;
 
 /**
  * @author hankun
  */
-public class SliderGeneratorImpl implements GeneratorInterface {
+public class SliderGeneratorImpl extends AbstractGenerator {
 
     @Override
     public CaptchaGeneratorVO generator() {
         CaptchaGeneratorVO captchaVO = new CaptchaGeneratorVO();
+
+
+
+
+
 
 
 
