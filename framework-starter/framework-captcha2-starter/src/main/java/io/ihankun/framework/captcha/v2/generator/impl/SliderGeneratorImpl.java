d528b1e7b34762e17cd4fc99979b9c639fbc03c5
韩坤 <hankun@msunhealth.com>
2024-04-28 14:23:22 +0800

    feature(2.0.0): 添加生成验证码接口

--- framework-starter/framework-captcha2-starter/src/main/java/io/ihankun/framework/captcha/v2/generator/impl/SliderGeneratorImpl.java
+++ framework-starter/framework-captcha2-starter/src/main/java/io/ihankun/framework/captcha/v2/generator/impl/SliderGeneratorImpl.java
@@ -3,29 +3,41 @@ package io.ihankun.framework.captcha.v2.generator.impl;
 import io.ihankun.framework.captcha.v2.generator.AbstractGenerator;
 import io.ihankun.framework.captcha.v2.generator.entity.CaptchaGeneratorVO;
 import io.ihankun.framework.core.utils.captcha.CaptchaImageUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
 
 /**
  * @author hankun
  */
+@Slf4j
+@Service
 public class SliderGeneratorImpl extends AbstractGenerator {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @Override
     public CaptchaGeneratorVO generator() {
         CaptchaGeneratorVO captchaVO = new CaptchaGeneratorVO();
 
         try {
-            ClassPathResource pathBackground = new ClassPathResource("/image/resource/1.jpg");
-            ClassPathResource pathFixedTemplate = new ClassPathResource("/image/template/1/fixed.png");
-            ClassPathResource pathActiveTemplate = new ClassPathResource("/image/template/1/active.png");
 
-            BufferedImage background = ImageIO.read(new File(pathBackground.getURI()));
-            BufferedImage fixedTemplate = ImageIO.read(new File(pathFixedTemplate.getURI()));
-            BufferedImage activeTemplate = ImageIO.read(new File(pathActiveTemplate.getURI()));
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            if (classLoader == null) {
+                classLoader = ClassLoader.getSystemClassLoader();
+            }
+            BufferedImage background = ImageIO.read(classLoader.getResourceAsStream("image/resource/1.jpg"));
+            BufferedImage fixedTemplate = ImageIO.read(classLoader.getResourceAsStream("image/template/1/fixed.png"));
+            BufferedImage activeTemplate = ImageIO.read(classLoader.getResourceAsStream("image/template/1/active.png"));
             BufferedImage maskTemplate = fixedTemplate;
 
             // 获取随机的 x 和 y 轴
@@ -35,7 +47,7 @@ public class SliderGeneratorImpl extends AbstractGenerator {
             BufferedImage cutImage = CaptchaImageUtils.cutImage(background, maskTemplate, randomX, randomY);
             CaptchaImageUtils.overlayImage(background, fixedTemplate, randomX, randomY);
 
-            // 这里创建一张png透明图片
+            // 创建一张png透明图片
             BufferedImage matrixTemplate = CaptchaImageUtils.createTransparentImage(activeTemplate.getWidth(), background.getHeight());
             CaptchaImageUtils.overlayImage(matrixTemplate, cutImage, 0, randomY);
 
