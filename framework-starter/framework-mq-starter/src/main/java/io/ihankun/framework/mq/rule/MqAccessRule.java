c3d1127ab04b80f5972a9afbac609ab75fa2577a
韩坤 <hankun@msunhealth.com>
2025-07-22 09:23:47 +0800

    feat: javax=》jakarta

--- framework-starter/framework-mq-starter/src/main/java/io/ihankun/framework/mq/rule/MqAccessRule.java
+++ framework-starter/framework-mq-starter/src/main/java/io/ihankun/framework/mq/rule/MqAccessRule.java
@@ -1,12 +1,11 @@
 package io.ihankun.framework.mq.rule;
 
 import io.ihankun.framework.mq.config.MqRuleProperties;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 
 /**
  * @author hankun
